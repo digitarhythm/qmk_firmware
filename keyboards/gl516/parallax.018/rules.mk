8abc831f53dbc79e98909b693d8a9d53840de72c
Hajime Oh-yake <kow@digitarhythm.net>
2023-09-18 01:11:31 +0900

    QMK firmware ver0.22.3 supported for gl516/parallax

--- keyboards/gl516/parallax.018/rules.mk
+++ keyboards/gl516/parallax.018/rules.mk
@@ -1,16 +1,29 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
 # Build Options
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # Enable N-Key Rollover
+NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no
 AUDIO_ENABLE = no           # Audio output
-
+GRAVE_ESC_ENABLE = yes
+LTO_ENABLE = yes
+SPACE_CADET_ENABLE = no
+MAGIC_ENABLE = no
+ENCODER_ENABLE = yes
 CUSTOM_MATRIX = lite
+
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+
+DEBOUNCE_TYPE = sym_eager_pk
 
 SRC += matrix.c
